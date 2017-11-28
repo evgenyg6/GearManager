598fd071532ea0756cd5447d19218d31c3748072
ilsilentii <travis.vdm@outlook.com>
2017-11-28 15:16:53 -0700

    Add files via upload

--- OAuth.js
+++ OAuth.js
@@ -2,6 +2,6 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
     if (window.location.href.indexOf("code") > -1) {
-      consolelog("found the code!!");
+      console.log("found the code!!");
     }
 });
