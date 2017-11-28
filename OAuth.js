73ab6018782550192ce2e90d55b3abe8f3e1860d
ilsilentii <travis.vdm@outlook.com>
2017-11-28 15:58:43 -0700

    Add files via upload

--- OAuth.js
+++ OAuth.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
     console.log( "ready!" );
 
     if (window.location.href.indexOf("code") > -1) {
-      console.log(window.location.pathname);
+      var url = window.location.href
+      var OAuth= url.split('?code=')[1]
+      console.log(OAuth);
       //window.location.replace("https://ilsilentii.github.io/GearManager/");
     }
 });
