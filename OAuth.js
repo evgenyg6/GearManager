09be31e347270e9416c6ef910e1abb3dcaf0d7a3
ilsilentii <travis.vdm@outlook.com>
2017-11-28 15:15:25 -0700

    Add files via upload

--- OAuth.js
+++ OAuth.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    if (window.location.href.indexOf("catalogue") > -1) {
-      consolelog("found the code!!")
+    if (window.location.href.indexOf("code") > -1) {
+      consolelog("found the code!!");
     }
 });
