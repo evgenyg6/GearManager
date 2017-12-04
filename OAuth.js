c52d60b4693442a1c148b35a7e85159b715eb981
ilsilentii <travis.vdm@outlook.com>
2017-12-03 19:30:12 -0700

    Add files via upload

---
+++ OAuth.js
@@ -0,0 +1,127 @@
+$(document).ready(function() {
+    var apiKey = "9a29535463e94dd284e033d5618eb1d5";
+
+    if (window.location.href.indexOf("code") > -1) {
+        var url = window.location.href
+        var OAuth = url.split('?code=')[1]
+        var data = {
+            grant_type: 'authorization_code',
+            code: OAuth,
+            client_id: '22868',
+            'X-API-Key': '9a29535463e94dd284e033d5618eb1d5'
+        }
+        authorize(data);
+    }
+
+    function authorize(data) {
+        $.ajax({
+            type: 'POST',
+            url: 'https://www.bungie.net/Platform/App/OAuth/Token/',
+            data: data,
+            success: function(object) {
+                const accessToken = Object.values(object)[0];
+                const headers = {
+                    Authorization: 'Bearer ' + accessToken,
+                    'X-API-Key': apiKey
+                };
+                console.log(headers);
+                getCurrentUser(headers);
+                getCurrency(headers);
+            },
+            error: function(err) {
+                console.log(err);
+            }
+        })
+    }
+
+    function getCurrentUser(headers) {
+        $.ajax({
+            type: 'GET',
+            headers: headers,
+            url: "https://www.bungie.net/Platform/User/GetMembershipsForCurrentUser/",
+            success: function(object) {
+                console.log(object)
+                var displayName = object[Object.keys(object)[0]].destinyMemberships[1].displayName
+                var membershipId = object[Object.keys(object)[0]].destinyMemberships[1].membershipId
+                var membershipType = object[Object.keys(object)[0]].destinyMemberships[1].membershipType
+                var getProfileURL = "https://www.bungie.net/Platform/Destiny2/" + membershipType + "/Profile/" + membershipId + "/?components=100"
+
+                $('<span>'+ displayName + '</span>').addClass('displayName').appendTo('#container')
+                getProfile(headers, getProfileURL);
+            },
+            error: function(err) {
+                console.log(err);
+            }
+        })
+    }
+
+    function getProfile(headers, getProfileURL) {
+        $.ajax({
+            type: 'GET',
+            headers: headers,
+            url: getProfileURL,
+            success: function(object) {
+                console.log(object);
+                var firstCharacter = object[Object.keys(object)[0]].profile.data.characterIds[0]
+                var secondCharacter = object[Object.keys(object)[0]].profile.data.characterIds[1]
+                var thirdCharacter = object[Object.keys(object)[0]].profile.data.characterIds[2]
+
+                getCharacters(headers, firstCharacter,  secondCharacter, thirdCharacter);
+            },
+            error: function(err) {
+                console.log(err);
+            }
+        })
+    }
+
+    function getCurrency(headers) {
+        $.ajax({
+            type: 'GET',
+            headers: headers,
+            url: 'https://www.bungie.net/Platform/Destiny2/4/Profile/4611686018467569204/?components=103',
+            success: function(object) {
+                console.log(object);
+                var glimmer = object[Object.keys(object)[0]].profileCurrencies.data.items[0].quantity
+                var shard = object[Object.keys(object)[0]].profileCurrencies.data.items[1].quantity
+                var brightdust = object[Object.keys(object)[0]].profileCurrencies.data.items[2].quantity
+
+                $('<span>'+ glimmer + '</span>').addClass('glimmer').appendTo('.nav');
+                $('<span>'+ shard + '</span>').addClass('shard').appendTo('.nav');
+                $('<span>'+ brightdust + '</span>').addClass('brightdust').appendTo('.nav');
+
+            },
+            error: function(err) {
+                console.log(err);
+            }
+        })
+    }
+
+    function getCharacters(headers, firstCharacter, secondCharacter, thirdCharacter) {
+        var firstCharacterString = firstCharacter.toString();
+        var secondCharacterString = secondCharacter.toString();
+        var thirdCharacterString = thirdCharacter.toString();
+
+        $.ajax({
+            type: 'GET',
+            headers: headers,
+            url: 'https://www.bungie.net/Platform/Destiny2/4/Profile/4611686018467569204/?components=200' ,
+            success: function(object) {
+                console.log(object);
+                var firstEmblem = object[Object.keys(object)[0]].characters.data['2305843009299232574'].emblemBackgroundPath
+
+                console.log(firstEmblem);
+
+                var imageUrl = "https://www.bungie.net" + firstEmblem
+
+                $('<div/>').addClass('firstCharacter').css('background-image', 'url(' + imageUrl + ')').appendTo('#container')
+            },
+            error: function(err) {
+                console.log(err);
+            }
+        })
+    }
+
+
+
+
+});
