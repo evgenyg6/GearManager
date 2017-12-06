0297fe756cfccdc6617e28bd0f2c919d332b1b2e
ilsilentii <travis.vdm@outlook.com>
2017-12-06 13:29:38 -0700

    Add files via upload

--- assets/scripts/equipmentstats.js
+++ assets/scripts/equipmentstats.js
@@ -5,6 +5,12 @@ $(document).ready(function() {
   var handling = 46
   var reload = 37
 
+  var impact1 = 29
+  var range1 = 78
+  var stability1 = 50
+  var handling1 = 50
+  var reload1 = 60
+
   $('#energy').on('mouseenter', function(e) {
     $('<div/>').addClass('gearContainer').appendTo('#container')
     $('<div/>').addClass('gearTitle').appendTo('.gearContainer')
@@ -91,4 +97,100 @@ $(document).ready(function() {
   $('#energy').on('mouseleave', function(e) {
     $('.gearContainer').remove();
   });
+
+  $('#kinetic').on('mouseenter', function(e) {
+    $('<div/>').addClass('gearContainer').appendTo('#container')
+    $('<div/>').addClass('gearTitle').appendTo('.gearContainer')
+    $('<div/>').addClass('gearProperties').appendTo('.gearContainer')
+    $('<div/>').addClass('gearStats').appendTo('.gearContainer')
+
+    $('<span>ORIGIN STORY</span>').addClass('gearName').appendTo('.gearTitle')
+    $('<span>Auto Rifle</span>').addClass('gearType').appendTo('.gearTitle')
+    $('<span>Legendary</span>').addClass('gearCommon').appendTo('.gearTitle')
+
+    $('<span>Requires Level 20</span>').addClass('gearRequire').appendTo('.gearProperties')
+    $('<img/>').attr('src', 'assets/media/images/arc_large.png').addClass('arcImage').appendTo('.gearProperties');
+    $('<span>277</span>').addClass('attackValue').appendTo('.gearProperties')
+    $('<span>ATTACK</span>').addClass('attack').appendTo('.gearProperties')
+    $('<span>Your inciting incident is their tragic ending.</span>').addClass('gearDescription').appendTo('.gearProperties')
+    $('<span>Impact</span>').addClass('statProperty').appendTo('.gearStats')
+    $('<span>Range</span>').addClass('statProperty').appendTo('.gearStats')
+    $('<span>Stability</span>').addClass('statProperty').appendTo('.gearStats')
+    $('<span>Handling</span>').addClass('statProperty').appendTo('.gearStats')
+    $('<span>Reload Speed</span>').addClass('statProperty').appendTo('.gearStats')
+    $('<span>Rounds Per Minute</span>').addClass('statProperty').appendTo('.gearStats')
+    $('<span>Magazine</span>').addClass('statProperty').appendTo('.gearStats')
+
+
+    $('<div/>').addClass('impact').appendTo('.gearStats')
+    $('<div/>').addClass('impactPercent').css({
+      'position': 'absolute',
+
+      'width': impact1.toString() + "%",
+      'height': '100%',
+      'background-color': 'white',
+    }).appendTo('.impact')
+
+    $('<div/>').addClass('range').appendTo('.gearStats')
+    $('<div/>').addClass('rangePercent').css({
+      'position': 'absolute',
+
+      'width': range1.toString() + "%",
+      'height': '100%',
+      'background-color': 'white',
+    }).appendTo('.range')
+
+    $('<div/>').addClass('stability').appendTo('.gearStats')
+    $('<div/>').addClass('stabilityPercent').css({
+      'position': 'absolute',
+
+      'width': stability1.toString() + "%",
+      'height': '100%',
+      'background-color': 'white',
+    }).appendTo('.stability')
+
+    $('<div/>').addClass('handling').appendTo('.gearStats')
+    $('<div/>').addClass('handlingPercent').css({
+      'position': 'absolute',
+
+      'width': handling1.toString() + "%",
+      'height': '100%',
+      'background-color': 'white',
+    }).appendTo('.handling')
+
+    $('<div/>').addClass('reload').appendTo('.gearStats')
+    $('<div/>').addClass('reloadPercent').css({
+      'position': 'absolute',
+
+      'width': reload1.toString() + "%",
+      'height': '100%',
+      'background-color': 'white',
+    }).appendTo('.reload')
+
+    $('<span>450</span>').addClass('rpm').appendTo('.gearStats')
+    $('<span>32</span>').addClass('magazine').appendTo('.gearStats')
+
+    $('#kinetic').on('mousemove', function(e) {
+      $('.gearContainer').css({
+        'position': 'absolute',
+        'width': '20%',
+        'height': '50%',
+        left: e.pageX + 50,
+        top: e.pageY + 1
+      })
+    });
+  });
+
+  $('#kinetic').on('mouseleave', function(e) {
+    $('.gearContainer').remove();
+  });
+
+
+
+
+
+
+
+
+
 });
